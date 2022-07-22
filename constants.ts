281369e3ef150c03b6884523673491bc44d8ee1b
lansen-git <lansen02@outlook.com>
2022-07-22 23:26:31 +0800

    first commit

---
+++ constants.ts
@@ -0,0 +1,14 @@
+import JSBI from 'jsbi'
+
+export const FACTORY_ADDRESS = '0x527ebd73a64b96100b7aae6bdde4b876947d370b'
+
+export const INIT_CODE_HASH = 'b0369ccfee4b3a363e7c2dc0c57e14420a950794a06924293fa8ec3b369bea39'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
